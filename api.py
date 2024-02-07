b48b3521ed8fafb9655578b0ba947316113ca5c0
Valery Galaganov <vgalaganov@aligntech.com>
2024-02-07 20:19:20 +0300

    Initial commit

---
+++ api.py
@@ -0,0 +1,387 @@
+import os.path
+
+import settings
+import requests
+from requests_toolbelt.multipart.encoder import MultipartEncoder
+from datetime import datetime
+
+
+class PetFriends:
+    """
+    API Help: https://petfriends.skillfactory.ru/apidocs/
+
+    Class contains methods to use that API
+    """
+    def __init__(self):
+        self.base_url = 'https://petfriends.skillfactory.ru/'
+        self.headers = {'Accept': 'application/json'}
+        self.my_pets = []
+        self.sess = requests.Session()
+        try:
+            if self.get_api_key():  # get api_key and save it in self.headers
+                print(f'authorized with auth_key {self.headers["auth_key"]}')
+                status_code, data = self.get_pets()
+                if status_code == 200 and type(data) == list:
+                    self.my_pets = data
+                elif status_code == 200 and type(data) == dict:
+                    self.my_pets.append(data)
+                else:
+                    print('error getting pets:', status_code, data)
+            else:
+                print(0, 'Unknown login error in __init__')
+        except Exception as e:
+            print(e)
+
+    def request(self, method: str, path: str, headers: str = '', data = None, params = None) -> tuple:
+        """
+        Send a request with authentication data (if exists) and returns the results
+        :param method: Required. request's method. Can be either 'GET', 'POST', 'PUT' or 'DELETE'
+        :param path: Required. path in addition to base_url to send request to
+        :param headers: Optional. custom headers (dict) or just 'Content-Type' field in headers (str).
+        :param data: Optional. request's body. Use MultipartEncoder() in case you need to send formData.
+        :param params: Optional. params to add to requests - usually GET's params added to url: ?param1=value1&param2=value2&...
+        :return: Response status code and response data
+        """
+        # allowed methods in REST API:
+        methods = ['GET', 'POST', 'PUT', 'DELETE']
+        # add base URL
+        url = self.base_url + path if type(path) == str else ''
+        if method.upper() not in methods:
+            return (0, f'Unclear request method: {method}')
+        if headers and type(headers) == dict:
+            headers = self.headers | headers
+        elif headers and type(headers) == str:
+            headers = self.headers | {'Content-Type': headers}
+        else:
+            headers = self.headers
+        if type(data) == MultipartEncoder:
+            headers['Content-Type'] = data.content_type
+        if not data:
+            data = None
+        if not params or type(params) != dict:
+            params = None
+        # print(f'DEBUG: Request: {method} to {url}:\nparams={params}\nheaders={headers}\ndata={data}')
+        resp = self.sess.request(method, url, params=params, data=data, headers=headers)
+        if resp.headers['content-type'] == 'application/json':
+            return resp.status_code, resp.json()
+        else:
+            return resp.status_code, resp.text
+
+    def open_file(self, pet_photo: str):
+        if not pet_photo:
+            raise FileNotFoundError('Create pet failed: empty photo URL')
+
+            # determine photo file MIME type
+        if '/' in pet_photo.replace('\\', '/'):
+            file_name = pet_photo[pet_photo.replace('\\', '/').rindex('/')+1:]
+        else:
+            file_name = pet_photo
+        ext = file_name[file_name.rindex('.') + 1:]
+        if ext in ['jpg', 'jpeg']:
+            photo_type = 'image/jpeg'
+        elif ext == '.png':
+            photo_type = 'image/x-png'
+        elif ext == '.gif':
+            photo_type = 'image/gif'
+        else:
+            raise FileNotFoundError('photo file extension not supported:', pet_photo)
+
+        # ready to send a request
+        try:
+            f = open(pet_photo, 'rb')
+        except FileNotFoundError:
+            try:
+                f = open(os.path.join(os.path.dirname(__file__), pet_photo), 'rb')
+            except FileNotFoundError as e:
+                raise FileNotFoundError('create pet error: photo file not found: ' + pet_photo)
+        return file_name, f, photo_type
+
+    @staticmethod
+    def print_dict(data: dict) -> str:
+        if type(data) == dict:
+            for k in data.keys():
+                if type(data[k]) == str and len(data[k]) > 127:
+                    data[k] = data[k][:124] + '...'
+                elif type(data[k]) == list:
+                    data[k] = PetFriends.print_list(data[k])
+        return str(data)
+
+    @staticmethod
+    def print_list(data: list) -> str:
+        if type(data) == list:
+            for i in range(len(data)):
+                if type(data[i]) == dict:
+                    data[i] = PetFriends.print_dict(data[i])
+                elif type(data[i]) == str and len(data[i]) > 127:
+                    data[i] = data[i][:124] + '...'
+        return str(data)
+
+    @staticmethod
+    def print_resp(request_type: str, status_code: int, data: any) -> None:
+        if status_code == 0:
+            print(request_type, 'error:', data)
+        else:
+            if type(data) == dict:
+                data = PetFriends.print_dict(data)
+            elif type(data) == list:
+                data = PetFriends.print_list(data)
+            elif type(data) == str and len(data) > 127:
+                data = data[:124] + '...'
+        print(request_type, 'status code:', status_code)
+        print(f'result = {data}')
+
+    def get_api_key(self,
+                    email: str = None,
+                    password: str = None,
+                    force: bool = False) -> tuple[int, str]:
+
+        if not force and 'auth_key' in self.headers: # Cached credinals
+            return 200, self.headers['auth_key']
+        email = email or settings.valid_email
+        password = password or settings.valid_password
+
+        headers = {'email': email, 'password': password}
+        auth_key = ''
+
+        resp = self.sess.get(f'{self.base_url}api/key', headers=headers)
+        if resp.status_code == 200:
+            is_json = resp.headers['content-type'] == 'application/json'
+            auth_key = resp.json()['key'] if is_json else resp.text
+            self.headers['auth_key'] = auth_key
+        return resp.status_code, auth_key
+
+
+    def get_pets(self, pet_id=None, auth_key=None, filter: str = 'my_pets', status_code=None) -> list:
+        """
+        get user's pets from server
+        :param pet_id: gets specific pet's data, if set. Otherwise gets all pets
+        :return: list object with found pets
+        """
+        if auth_key == None and 'auth_key' not in self.headers:
+            return 0, 'Get pets error: need a new key'
+        headers = {}
+        if type(auth_key) == str:
+            headers['auth_key'] = auth_key
+        if filter != 'my_pets':
+            filter = ''
+        resp = self.request('GET', 'api/pets', params={'filter': filter}, headers=headers)
+        if resp[0] == 403 and auth_key == None:
+            del self.headers['auth_key']
+        else:
+            if type(resp[1]) == dict and 'pets' in resp[1].keys() and type(resp[1]['pets']) == list:
+                if filter == 'my_pets':
+                    self.my_pets = resp[1]['pets']
+                if pet_id:
+                    pets = list(p for p in resp[1]['pets'] if type(p) == dict and 'id' in p.keys() and p['id'] == pet_id)
+                    return status_code, pets
+                else:
+                    return resp[0], resp[1]['pets']
+        return resp
+
+    def create_pet(self, name: str, animal_type: str, age: int, pet_photo: str, bypass: bool = False) -> tuple:
+        """
+        creates a new pet
+        :param name: pet's name
+        :param animal_type: animal type (for ex. dog, cat, etc.)
+        :param age: pet's age in years
+        :param pet_photo: pet's photo file name
+        :param bypass: Optional. For tests ONLY. Ignore checks and bypass data to API
+        :return: returns response' status code and new pet's data
+        """
+        if bypass:
+            pass
+        elif 'auth_key' not in self.headers:
+            return 0, 'Create pet failed: user not authorized'
+        elif not name:
+            return 0, 'Create pet failed: name is empty'
+        elif type(age) == int and (age < 0 or age > 20) or type(age) == str and (int(age) < 0 or int(age) > 30):
+            return 0, 'Create pet failed: wrong age'
+        elif not animal_type:
+            return 0, 'Create pet failed: animal_type is empty'
+
+        # check pet_photo variable and get: [0] real file_name, [1] binary file, [2] MIME type
+        try:
+            pet_file = self.open_file(pet_photo)
+        except FileNotFoundError:
+            if bypass:
+                pet_file = ''
+            else:
+                return 0, f'Error create pet: photo file {pet_photo} not found'
+        data = MultipartEncoder(
+            fields={
+                'name': name if not bypass or name else '',
+                'animal_type': animal_type if not bypass or animal_type else '',
+                'age': str(age) if not bypass or age else '',
+                'pet_photo': (pet_file[0], pet_file[1], pet_file[2]) if pet_file else ''
+            }
+        )
+        resp = self.request('POST', 'api/pets', data=data)
+        if resp[0] == 200:
+            if type(resp[1]) == list:
+                self.my_pets.append(resp[1])
+                return resp[0], [p['id'] for p in resp[1]]
+
+            if type(resp[1]) == dict:
+                if 'id' in resp[1].keys() and 'created_at in resp[1].keys()':
+                    self.my_pets.append(resp[1])
+        elif resp[0] == 403:
+            del self.headers['auth_key']
+        return resp
+
+    def update_pet(self, pet_id, name = None, animal_type = None, age = None, bypass: bool = False):
+        """
+        Updates existing pet. At least, one of parameters (name, animal_type, or age) should be set
+        :param pet_id: existing pet_id, required
+        :param name: new name
+        :param animal_type: new animal_type
+        :param age: new age
+        :param bypass: Optional. For tests ONLY! Ignore checks and bypass data to API.
+        :return: returns updated pet's data
+        """
+        if 'auth_key' not in self.headers:
+            return (0, 'Update pet error: unauthorized. Missing auth_key')
+        if bypass:
+            pass
+        elif not self.my_pets or pet_id not in [p['id'] for p in self.my_pets]: # check if pet exists
+            return (0, 'Update pet error: pet not found. Pet_id = ' + str(pet_id))
+        elif not name and not animal_type and not age: # check if at least one parameter is given
+            return (0, 'Update pet error: nothing to update. Missing parameters')
+        try:
+            pet_index = [p['id'] for p in self.my_pets].index(pet_id)
+        except Exception:
+            if not bypass:
+                return 0, 'Updatepet error. pet not found. pet_id = ' + str(pet_id)
+        fields = {
+            'name': name if name else (self.my_pets[pet_index]['name'] if not bypass else str(name)),
+            'animal_type': animal_type if animal_type else (self.my_pets[pet_index]['animal_type'] if not bypass \
+                                                            else str(animal_type)),
+            'age': str(age) if age else (self.my_pets[pet_index]['age'] if not bypass else str(age))
+        } # prepare fields for request's formData
+        data = MultipartEncoder(fields=fields)
+        # print(f'\nold pet\'s data:\n{self.my_pets[pet_index]}\nreq = {req}')
+        resp = self.request('PUT', 'api/pets/' + str(pet_id), data=data)
+
+        if resp[0] == 200 and type(resp[1]) in [dict, list]:
+            pet_index = [p['id'] for p in self.my_pets].index(pet_id)
+            self.my_pets[pet_index] = resp[1]
+            ex_text = 'Update pet error: wrong received data:'
+            if name and resp[1]['name'] != name:
+                ex_text += f'\n - wrong name: \'{name}\' --> \'{resp[1]["name"]}\''
+            if animal_type and resp[1]['animal_type'] != animal_type:
+                ex_text += f'\n - wrong animal type: \'{animal_type}\' --> \'{resp[1]["animal_type"]}\''
+            if age and resp[1]['age'] != str(age):
+                ex_text += f'\n - wrong age: {age} --> {resp[1]["age"]}'
+            if pet_id != resp[1]['id']:
+                ex_text += f'\n - wrong ID: {pet_id} --> {resp[1]["id"]}'
+            if len(ex_text) > 40:
+                return (0, ex_text)
+            return resp
+        elif resp.status_code == 403:
+            del self.headers['auth_key']
+            return resp
+        else:
+            return (0, 'Update pet error ' + str(resp[0]) + ': ' + str(resp[1]))
+
+    def delete_pet(self, pet_id=None) -> None:
+        """
+        deletes pet from system
+        :param pet_id: pet_id to delete
+        :return:
+        """
+        # if type(self.my_pets) != list:
+        #     self.my_pets = []
+        if not pet_id and len(self.my_pets) > 0:
+            # print(f'my_pets = ({type(self.my_pets)}) {self.my_pets}')
+            pet_id = self.my_pets[0]['id']
+        elif not pet_id:
+            return (0, 'Pet deletion failed: cannot find pet_id to delete. '
+                                      'Probably pet\'s already deleted from the shop')
+
+        resp = self.request('DELETE', 'api/pets/' + str(pet_id))
+        if resp[0] == 200:
+            # find pet and delete it
+            if pet_id in list(map(lambda p: p['id'], self.my_pets)):
+                idx = list(i for i in range(len(self.my_pets)) if self.my_pets[i]['id'] == pet_id)
+                for i in idx[::-1]:
+                    self.my_pets.pop(i)
+        return resp
+
+    def create_pet_simple(self, name: str, animal_type: str, age: str, bypass: bool = False):
+        """
+        Creates a new pet without photo
+        :param name: pet's name
+        :param animal_type: pet's type (dog, cat, etc.)
+        :param age: pet's age
+        :param bypass: Optional. For tests ONLY. Ignore simple checks and bypass data to API.
+        :return: response's status_code and data
+        """
+        if not bypass and (not name or not animal_type or not age):
+            return (0, f'values may not be empty:\n - name: {name}, animal_type: {animal_type}, age: {age}')
+        data = MultipartEncoder(
+            fields={
+                'name': name,
+                'animal_type': animal_type,
+                'age': str(age)
+            }
+        )
+        resp = self.request('POST', 'api/create_pet_simple', data=data)
+        if resp[0] == 200 and type(resp[1]) == dict and 'id' in resp[1].keys():
+            self.my_pets.append(resp[1])
+        return resp
+
+    def add_photo(self, pet_id: str, pet_photo: str, bypass: bool = False):
+        """
+        Adds / changes photo of pet
+        :param pet_id: pet_id to add photo to
+        :param pet_photo: photo file name
+        :param bypass: Optional. For tests ONLY. Ignore checks and bypass data to server
+        :return: response's status_code and data
+        """
+        try:
+            f = self.open_file(pet_photo)
+        except Exception as e:
+            if not bypass:
+                return 0, e
+            data = MultipartEncoder(fields={'pet_photo': ''})
+        else:
+            data = MultipartEncoder(fields={'pet_photo': f})
+        return self.request('POST', 'api/pets/set_photo/' + str(pet_id), data=data)
+
+    def print_pets(self, pet=None) -> str:
+        """
+        Creates a string with pet's data
+        :param pet: specific pets to print. If empty, method prints all pets' data
+        :return: formatted string with pet's data.
+        """
+        pets = ''
+        if pet and type(pet) == list:
+            for p in pet:
+                if pets:
+                    pets += '\n'
+                pets = self.print_pets(p)
+        elif pet and type(pet) == dict:
+            pets = '{'
+            for k in pet.items():
+                if k[0] in ['id', 'age', 'animal_type', 'name']:
+                    pets += '\n    \'' + k[0] + '\': \'' + k[1] + '\''
+                elif k[0] == 'created_at':
+                    pets += '\n    \'' + k[0] + '\': \'' + str(datetime.fromtimestamp(float(k[1]))) + '\''
+                else:
+                    pets += '\n    \'' + k[0] + '\': \'' + (k[1] if len(k[1]) < 128 else k[1][:125] + '...') + '\''
+            pets += '\n}' if len(pets) > 3 else '}'
+        elif len(self.my_pets) > 0:
+            for p in self.my_pets:
+                if pets:
+                    pets += '\n'
+                if p and type(p) == dict:
+                    pets += '{'
+                    for k in p.items():
+                        if k[0] in ['id', 'age', 'animal_type', 'name']:
+                            pets += '\n    \'' + k[0] + '\': \'' + k[1] + '\''
+                        elif k[0] == 'created_at':
+                            pets += '\n    \'' + k[0] + '\': \'' + str(datetime.fromtimestamp(float(k[1]))) + '\''
+                        else:
+                            pets += '\n    \'' + k[0] + '\': \'' + (k[1] if len(k[1]) < 128 else k[1][:125]+'...') + '\''
+                    pets += '\n}'
+        return pets
+
---
+++ tests/test_pet_friends.py
@@ -0,0 +1,241 @@
+import settings
+from api import PetFriends
+from settings import valid_email, valid_password
+
+from requests_toolbelt import MultipartEncoder
+import os
+import pytest
+
+pf = PetFriends()
+
+
+def test_get_api_key_for_valid_user():
+    """
+    Получаем ключ auth_key для верных данных (email, password)
+    :param email: valid_email
+    :param password: valid_password
+    :return:
+    """
+    # Отправляем запрос и сохраняем полученный ответ с кодом статуса в status, а текст ответа в result
+    status, _ = pf.get_api_key(email=settings.valid_email,
+                               password=settings.valid_password,
+                               force=True)
+
+    # Сверяем полученные данные с нашими ожиданиями
+    assert status == 200
+    assert type(_) is str and len(_) > 0
+
+def test_get_api_key_for_invalid_user():
+    """
+    Получаем ключ auth_key для неверных данных (email, password)
+    :param email: invalid_email
+    :param password: invalid_password
+    :return:
+    """
+    # Отправляем запрос и сохраняем полученный ответ с кодом статуса в status, а текст ответа в result
+    status, _ = pf.get_api_key(email=settings.invalid_email,
+                               password=settings.invalid_password,
+                               force=True)
+
+    # Сверяем полученные данные с нашими ожиданиями
+    assert status == 403
+    assert type(_) is str and len(_) == 0
+
+def test_get_all_pets_with_valid_key(filter=''):
+    """ Проверяем что запрос всех питомцев возвращает не пустой список.
+    Для этого сначала получаем api ключ и сохраняем в переменную auth_key. Далее используя этого ключ
+    запрашиваем список всех питомцев и проверяем что список не пустой.
+    Доступное значение параметра filter - 'my_pets' либо '' """
+
+    # проверяем, есть ли действительный ключ api_key
+    if 'auth_key' not in pf.headers:
+        pf.get_api_key() # если нет, авторизуемся
+
+    # отправляем запрос на получение всех питомцев
+    status_code, list_pets = pf.get_pets(filter=filter)
+
+    # проверяем статус и тело ответа
+    assert status_code == 200 and type(list_pets) == list
+    # тело должно быть непустым списком, и в первом питомце должно быть поле 'id' (как минимум)
+    assert len(list_pets) >= 0
+    if len(list_pets) > 0:
+        assert type(list_pets[0]) == dict
+        assert 'id' in list_pets[0].keys()
+
+def test_get_pets_w_invalid_key(api_key = 'none'):
+    """
+    проверяем запрос на питомцев с неправильной авторизацией
+    :param api_key: ключ с неправильной авторизацией
+    :return:
+    """
+    # отправляем запрос на получение питомцев с заведомо неверным ключом
+    status_code, list_pet = pf.get_pets(auth_key=api_key)
+    # код ответа должен быть 403
+    assert status_code == 403
+
+def test_get_my_pets():
+    # отправляем запрос на получение питомцев
+    status, resp = pf.get_pets(filter='')
+
+    # проверяем статус и тип тела ответа
+    assert status == 200 and type(resp) == list
+
+def test_add_new_pet_with_valid_data(name='Барбоскин', animal_type='двортерьер', age='4', pet_photo='images/IMG_8496.jpeg'):
+    """Проверяем что можно добавить питомца с корректными данными"""
+    # Получаем полный путь изображения питомца и сохраняем в переменную pet_photo
+    pet_photo = os.path.join(os.path.dirname(__file__), pet_photo)
+
+    # Добавляем питомца
+    status, new_pet = pf.create_pet(name, animal_type, age, pet_photo)
+
+    # проверяем статус ответа
+    assert status == 200
+    # Сверяем полученный ответ с ожидаемым результатом
+    assert type(new_pet) == dict
+    assert 'name' in new_pet.keys() and new_pet['name'] == name
+
+def test_add_new_pet_with_partial_data(name='', animal_type='соня', age=-3, pet_photo='', bypass=True):
+    """Проверяем что можно добавить питомца с корректными данными"""
+    # Добавляем питомца
+    status, new_pet = pf.create_pet(name, animal_type, age, pet_photo, bypass=bypass)
+
+    # Сверяем полученный ответ с ожидаемым результатом
+    assert status == 400
+
+def test_add_new_pet_with_invalid_data(name='\n', animal_type='\t', age=-500, pet_photo='', bypass=True):
+    """Проверяем что можно добавить питомца с корректными данными"""
+    # Добавляем питомца
+    status, new_pet = pf.create_pet(name, animal_type, age, pet_photo, bypass=True)
+
+    # Сверяем полученный ответ с ожидаемым результатом
+    assert status == 400
+
+def test_create_pet_simple_correct(name='Котяра', animal_type='Сибиряк', age=2):
+    # отправляем запрос на нового питомца
+    status, resp = pf.create_pet_simple(name, animal_type, age)
+
+    # Проверяем статус и тип тела ответа
+    assert status == 200
+    assert type(resp) in [list, dict]
+
+def test_create_pet_simple_invalid(method='POST', path='api/create_pet_simple', body={'name': '', 'age':'3'}):
+    """
+    Проверяем неправильный запрос на создание питомца (simple).
+    Запрос генерируется в самом тесте, не в api, поэтому берём на входе нужные для него данные с неправильным body
+    :param method: метод должен быть POST
+    :param path: относительный путь запроса: 'api/create_pet_simple'
+    :param body: в body должно отсутствовать требуемое поле
+    :return:
+    """
+    # в API функция отправляет все поля; мы же должны отправить не все. Поэтому пишем новый запрос
+    # подготавливаем и отправляем запрос
+    data = MultipartEncoder(fields=body)
+    status, _ = pf.request(method, path, data.content_type, data)
+
+    # проверяем статус
+    assert status == 400
+
+def test_update_pet(name='Мурзик', animal_type='Котэ', age=5):
+    """Проверяем возможность обновления информации о питомце"""
+
+    # Получаем ключ auth_key и запрашиваем список своих питомцев
+    _, my_pets = pf.get_pets()
+
+    # Проверяем - если список своих питомцев пустой, то добавляем нового и опять запрашиваем список своих питомцев
+    if len(my_pets) == 0:
+        _, pet = pf.create_pet("Суперкот", "кот", "3", "images/IMG_8496.jpeg")
+        my_pets = [pet]
+
+    # посылаем запрос на обновление
+    status, result = pf.update_pet(my_pets[0]['id'], name, animal_type, age)
+
+    # Проверяем что статус ответа = 200 и имя питомца соответствует заданному
+    assert status == 200
+    assert result['name'] == name
+    assert result['animal_type'] == animal_type
+    assert result['age'] == str(age)
+
+def test_add_invalid_photo(pet_photo=''):
+    """Проверяем запрос на добавление фото с пустым фото"""
+    # берём список своих питомцев
+    my_pets = pf.get_pets()
+    # и ищем ID питомца без фото
+    pet_id = None
+    if type(my_pets) == list and len(my_pets) > 0:
+        for i in range(len(pf.my_pets)):
+            if type(my_pets[i]) == dict and 'id' in my_pets[i].keys() and 'pet_photo' in my_pets[i].keys() \
+                and my_pets[i]['pet_photo'] == '':
+                pet_id = i
+                break
+    # если питомца без фото нет, создаём его
+    if pet_id == None:
+        _, pets = pf.create_pet_simple('Мурзилка', 'котик', '3')
+        pet_id = pets['id']
+
+    # отправляем запрос на добаление пустого фото с опцией bypass, чтобы игнорировать явную ошибку (для теста)
+    status, resp = pf.add_photo(pet_id, pet_photo, bypass=True)
+
+    # проверяем, что статус = 400
+    assert status == 400
+
+def test_add_valid_photo(pet_photo='images/IMG_8496.jpeg'):
+    # получаем список своих питомцев
+    my_pets = pf.get_pets()
+
+    # пытаемся найти ID питомца без фото
+    pet_id = None
+    if type(my_pets) == list and len(my_pets) > 0:
+        for i in range(len(pf.my_pets)):
+            if type(my_pets[i]) == dict and 'id' in my_pets[i].keys() and 'pet_photo' in my_pets[i].keys() \
+                and my_pets[i]['pet_photo'] == '':
+                pet_id = i
+                break
+    # если не нашли, создаём нового питомца без фото
+    if pet_id == None:
+        _, pets = pf.create_pet_simple('Мурзилка', 'котик', '3')
+        pet_id = pets['id']
+
+    # добавляем фото
+    status, resp = pf.add_photo(pet_id, pet_photo)
+
+    # проверяем статус и наличие фото
+    assert status == 200
+    assert resp['pet_photo'][:23] == 'data:image/jpeg;base64,'
+
+def test_delete_self_pet():
+    """Проверяем возможность удаления питомца"""
+
+    # Получаем ключ auth_key и запрашиваем список своих питомцев
+    _, my_pets = pf.get_pets()
+
+    # Проверяем - если список своих питомцев пустой, то добавляем нового и опять запрашиваем список своих питомцев
+    if len(my_pets) == 0:
+        _, pet = pf.create_pet("Суперкот", "кот", "3", "images/IMG_8496.jpeg")
+        my_pets = [pet]
+
+    # Берём id первого питомца из списка и отправляем запрос на удаление
+    pet_id = my_pets[0]['id']
+
+    # Ещё раз запрашиваем список своих питомцев
+    _, my_pets = pf.get_pets()
+
+    # Проверяем что в списке питомцев нет id удалённого питомца
+    pet_ids = [p['id'] for p in my_pets]
+    assert pet_id is not None
+    assert pet_id not in my_pets
+
+def test_delete_pet_w_incorrect_id(pet_id = '99999999999'):
+    """Проверяем возможность удаления питомца"""
+
+    # запрашиваем список своих питомцев
+    _, my_pets = pf.get_pets()
+
+    # если есть питомцы, проверяем, что питомца нет среди моих питомцев:
+    if len(my_pets) > 0:
+        assert pet_id not in list(map(lambda x: x['id'], my_pets))
+
+    # удаляем питомца с неправильным ключом
+    status, resp = pf.delete_pet(pet_id)
+
+    # Согласно докам, код ответа == 200, если пет был удалён (даже если его и не существовало)
+    assert status == 200
